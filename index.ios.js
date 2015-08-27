8389baa82d2dd940335a68b675952a382422a94f
Clayton Smith <clayton.smith@shopify.com>
2015-08-27 12:58:19 -0400

    Buses on a map!

--- index.ios.js
+++ index.ios.js
@@ -13,24 +13,73 @@ var {
   View,
 } = React;
 
+var appID = '1d5a86db';
+var apiKey = '336c4052a44a741a4b0344060ef73b12';
+
+var ALL_ROUTES_URL = 'https://api.octranspo1.com/v1.2/GetNextTripsForStopAllRoutes'
+
 var region = {
   latitude: 45.420591,
   longitude: -75.692859,
-  latitudeDelta: 0.02,
-  longitudeDelta: 0.02,
+  latitudeDelta: 0.1,
+  longitudeDelta: 0.1,
 };
 
 var BusFollower = React.createClass({
+  getInitialState: function() {
+    return {
+      annotations: null,
+    }
+  },
+
   render: function() {
     return (
       <View style={styles.container}>
         <MapView
           style={styles.map}
           region={region}
+          annotations={this.state.annotations}
         />
       </View>
     );
-  }
+  },
+
+  componentDidMount: function() {
+    this.fetchData();
+  },
+
+  fetchData: function() {
+    var params = {
+      method: 'POST',
+      body: 'appID=' + appID + '&apiKey=' + apiKey + '&stopNo=' + '3001' + '&format=json',
+    };
+    fetch(ALL_ROUTES_URL, params)
+      .then((response) => response.json())
+      .then((responseData) => {
+        var routes = responseData.GetRouteSummaryForStopResult.Routes.Route;
+        var annotations = [];
+        for (var i = 0; i < routes.length; i++) {
+          if (routes[i].Trips) {
+            var trips = routes[i].Trips;
+            var routeNo = routes[i].RouteNo;
+            for (var j = 0; j < trips.length; j++) {
+              var trip = trips[j];
+              if (trip.Latitude) {
+                annotations.push({
+                  latitude: parseFloat(trip.Latitude),
+                  longitude: parseFloat(trip.Longitude),
+                  title: routeNo,
+                });
+              }
+            }
+          }
+        }
+        this.setState({
+          annotations: annotations,
+        });
+      })
+      .done();
+  },
 });
 
 var styles = StyleSheet.create({
